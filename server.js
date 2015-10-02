5160b118ff9c61d4eb4763c177f5cf5bbefddac9
Alfredo Miranda <alfredo@miranda.io>
2015-10-02 14:53:12 -0500

    Add angular, gitignore and other goodies

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-  res.end('hey there');
-});
+app.use(express.static('views'));
+app.use(express.static('bower_components'));
 
 app.listen(port);
